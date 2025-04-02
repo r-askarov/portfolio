99e2b705e266a997c6bb9801c71078d58b1a654e
r-askarov <roy.askarov@gmail.com>
2025-04-02 14:52:26 -0500

    Removed unnecessary files from repo

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: "class", // Enables class-based dark mode
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
