921d2763c6cf7142008c10b6db69d54fd544af0e
r-askarov <roy.askarov@gmail.com>
2025-04-11 15:37:29 -0500

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // Enables class-based dark mode
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lightBg: "#9D174D", // Indigo color
+      }
+    },
+  },
+  plugins: [],
+};
